7f53fd5570577c1b0bd95d66f12a7a00a2cc2c7e
Cody LaFlamme <claflamme123@gmail.com>
2021-05-05 10:30:50 -0400

    Adds baselines req. plot; updates others

--- linear_model.py
+++ linear_model.py
@@ -9,12 +9,13 @@ import random
 import numpy as np
 from scipy.interpolate import interp1d #for interpolation
 import scipy.integrate #for shaded area calculation
-
+import datetime as dt
+from scipy.stats import gaussian_kde #for kernel estimation
 from mpl_toolkits.mplot3d import Axes3D  # for 3d projection plots
 
 #======== PLOTTING CONFIG +=========
 FONT_SIZE = 11
-SMALL_FIG_FONT_SIZE = 15
+SMALL_FIG_FONT_SIZE = 16
 plt.rcParams.update({'font.size': FONT_SIZE})
 
 # ======= CONSTANTS ==========
@@ -27,7 +28,7 @@ INTERP_DATA = False #interpolates waveforms into a spline before solving
 INTERP_VISUAL = False #interpolates waveforms into a spline AFTER solving/simulating (if interp_data=True, this is ignored)
 NORMALIZE=True
 
-COMBINED_PLOT = False
+COMBINED_PLOT = True
 PAST_PLOT = False
 REFLECTION_PLOT = True
 
@@ -78,7 +79,7 @@ if INTERP_DATA:
     wfs = np.array([interp1d(x,wf,kind='cubic')(xx) for wf in wfs])
     
 #============ SPLIT DATA ==============
-N_winter = 14601
+N_winter = 14595
 N_summer = N_env-N_winter
 x_full = env_data
 y_full = wfs
@@ -134,31 +135,51 @@ for i in range(N_full):
 
 p_mse = np.mean((p_full-y_full)**2,axis=1)
 
-#zoomed
+#zoomed CC plot
 #plt.figure()
+sample_gap = 2000 #gap between summer and winter, in samples
+xtick_gap = 4*24*3600 #in seconds
+cc_x = np.arange(0,N_env)
+cc_x[N_winter:] += sample_gap
+time_xticks = []
+time_xticklabels = []
+last_tick = times[0]
+for i,t in enumerate(times):
+    if t-last_tick > xtick_gap:
+        last_tick = t
+        time_xticks.append(cc_x[i])
+        date_str = str(dt.datetime.fromtimestamp(t,dt.timezone.utc).date())
+        time_xticklabels.append(date_str)
 lw=1
 fig,ax1 = plt.subplots()
-#plot training
-train_segments = np.split(train_indices,np.where(np.diff(train_indices)!=1)[0]+1)
+ax1.set_xticks(time_xticks)
+ax1.set_xticklabels(time_xticklabels, rotation=45, ha='right', rotation_mode="anchor")
+#plot training: split training set into multiple parts with borders at index discontinuities and the season border (if present). this prevents lines from being drawn across long gaps
+train_segments = np.split(train_indices,np.where(np.logical_or(np.diff(train_indices)!=1, train_indices[1:]==N_winter))[0]+1) #index train_indices at [1:] to match size with diff results. add 1 to offset this movement
 for seg in train_segments:
-    lt1, = ax1.plot(seg,p_cc[seg], label="Training CC",color="C0",lw=lw)
-#plot testing
-test_segments = np.split(test_indices,np.where(np.diff(test_indices)!=1)[0]+1)
+    lt1, = ax1.plot(cc_x[seg],p_cc[seg], label="Training CC",color="C0",lw=lw)
+#plot testing: split as training set is above
+test_segments = np.split(test_indices,np.where(np.logical_or(np.diff(test_indices)!=1, test_indices[1:]==N_winter))[0]+1)
 for seg in test_segments:
-    lt2, = ax1.plot(seg, p_cc[seg], label="Testing CC",color="C1",lw=lw)
-
+    lt2, = ax1.plot(cc_x[seg], p_cc[seg], label="Testing CC",color="C1",lw=lw)
 ax1.set_ylabel("CC")
-ax1.set_xlabel("Sample")
+ax1.set_xlabel("Date")
 ax2 = ax1.twinx()
-li, = ax2.plot(illum,label="Illuminance",alpha=0.3, lw=1,color="C2")
-ax2.fill_between(range(N_full),illum,np.ones(N_full)*np.min(illum), alpha=0.2, color="C2")
+#plot illuminance
+#li, = ax2.plot(cc_x, illum,label="Illuminance",alpha=0.3, lw=1,color="C2")
+li, = ax2.plot(cc_x[:N_winter], illum[:N_winter],label="Illuminance",alpha=0.3, lw=1,color="C2")
+li, = ax2.plot(cc_x[N_winter:], illum[N_winter:],label="Illuminance",alpha=0.3, lw=1,color="C2")
+#ax2.fill_between(range(N_full),illum,np.ones(N_full)*np.min(illum), alpha=0.2, color="C2")
+ax2.fill_between(cc_x[:N_winter],illum[:N_winter],np.ones(N_winter)*np.min(illum), alpha=0.2, color="C2")
+ax2.fill_between(cc_x[N_winter:],illum[N_winter:],np.ones(N_summer)*np.min(illum), alpha=0.2, color="C2")
 ax2.set_ylabel("Log10 Illuminance (log Lux)")
 plt.legend((lt1,lt2,li),("Training CC","Testing CC","Illuminance"),loc="lower left")
+#plt.legend((lt1,lt2,li),("Training CC","Testing CC","Illuminance"),loc=(1,0))
 plt.title("Linear Model Prediction CC\nTrain mean CC: {:.4f}\n Test mean CC: {:.4f}".format(np.mean(p_cc[train_indices]), np.mean(p_cc[test_indices])))
 plt.tight_layout()
-plt.savefig("{:s}/{:s}_cc_full_zoomed".format(out_folder,desc_str))
+plt.savefig("{:s}/{:s}_cc_full_zoomed".format(out_folder,desc_str)) #zoomed on plotted region
 ax1.set_ylim((0,1))
-plt.savefig("{:s}/{:s}_cc_full".format(out_folder,desc_str))
+plt.savefig("{:s}/{:s}_cc_full".format(out_folder,desc_str)) #full 0-1 CC range
 
 
 #violin plots showing environment distribution
@@ -188,6 +209,8 @@ for e,c,n,u in zip(env_sets,colors,names,units):
     plt.tight_layout()
     plt.savefig("{:s}/distribution_{:s}".format(out_folder,n.lower()))
 plt.rcParams.update({'font.size': FONT_SIZE})
+
+
 #temperature/illuminance plot
 plt.figure()
 plt.title("Temperature and Illuminance in Measured Data")
@@ -199,37 +222,46 @@ plt.savefig("{:s}/temp_illum_comparison".format(out_folder))
 #=========== plotting variation from each factor ====================
 
 #find mode daytime illuminance
-illum_counts, illum_centers = np.histogram(illum)
-illum_mode_bin_i = np.argmax(illum_counts)
-illum_mode = illum_centers[illum_mode_bin_i]
+#illum_counts, illum_centers = np.histogram(illum)
+#illum_mode_bin_i = np.argmax(illum_counts)
+#illum_mode = illum_centers[illum_mode_bin_i]
+illum_x = np.linspace(np.min(illum),np.max(illum),N_simulations);
+illum_kernel = gaussian_kde(illum)
+illum_mode = illum_x[np.argmax(illum_kernel.pdf(illum_x))]
 
 #find mode temperature
-degC_counts, degC_centers = np.histogram(degC)
-degC_mode_bin_i = np.argmax(degC_counts)
-degC_mode = degC_centers[degC_mode_bin_i]
+#degC_counts, degC_centers = np.histogram(degC)
+#degC_mode_bin_i = np.argmax(degC_counts)
+#degC_mode = degC_centers[degC_mode_bin_i]
+degC_x = np.linspace(np.min(degC),np.max(degC),N_simulations);
+degC_kernel = gaussian_kde(degC)
+degC_mode = degC_x[np.argmax(degC_kernel.pdf(degC_x))]
 
 #find mode humidity
-RH_counts, RH_centers = np.histogram(RH)
-RH_mode_bin_i = np.argmax(RH_counts)
-RH_mode = RH_centers[RH_mode_bin_i]
+#RH_counts, RH_centers = np.histogram(RH)
+#RH_mode_bin_i = np.argmax(RH_counts)
+#RH_mode = RH_centers[RH_mode_bin_i]
+RH_x = np.linspace(np.min(RH),np.max(RH),N_simulations);
+RH_kernel = gaussian_kde(RH)
+RH_mode = RH_x[np.argmax(RH_kernel.pdf(RH_x))]
 
 #Illuminance: fix temperature and humidity
 illum_I = np.ones((N_simulations,ENV_SIZE))
-illum_I[:,illum_index] = np.linspace(np.min(illum),np.max(illum),N_simulations);
+illum_I[:,illum_index] = illum_x
 illum_I[:,degC_index] = degC_mode
 illum_I[:,RH_index] = RH_mode
 illum_Y = illum_I@M
 
 degC_I = np.ones((N_simulations,ENV_SIZE))
 degC_I[:,illum_index] = illum_mode
-degC_I[:,degC_index] = np.linspace(np.min(degC),np.max(degC),N_simulations);
+degC_I[:,degC_index] = degC_x
 degC_I[:,RH_index] = RH_mode
 degC_Y = degC_I@M
 
 RH_I = np.ones((N_simulations,ENV_SIZE))
 RH_I[:,illum_index] = illum_mode
 RH_I[:,degC_index] = degC_mode
-RH_I[:,RH_index] = np.linspace(np.min(RH),np.max(RH),N_simulations);
+RH_I[:,RH_index] = RH_x
 RH_Y = RH_I @ M
 
 N_COMBINED_SIMULATIONS = 100 #we get a number of pints equal to this number CUBED
@@ -254,13 +286,14 @@ if INTERP_VISUAL and not INTERP_DATA:
     RH_Y    = np.array([interp1d(x,wf,kind='cubic')(xx) for wf in RH_Y])
     combined_Y    = np.array([interp1d(x,wf,kind='cubic')(xx) for wf in combined_Y])
 
+plt.rcParams.update({'font.size': SMALL_FIG_FONT_SIZE})
 #each plot will share the same code. extra initial investment, but reduces headaches later.
 mode_strings = ("Illuminance: 10^{:.2f} Lux".format(illum_mode), "Temperature: {:.2f} C".format(degC_mode), "Humidity: {:.2f} %".format(RH_mode))
 lw = 1
 for i,(name,Y) in enumerate(zip(("Illuminance","Temperature","Humidity","All"),(illum_Y,degC_Y,RH_Y,combined_Y))):
     if i!=3 or COMBINED_PLOT:
         used_m_strings = [m for j,m in enumerate(mode_strings) if i!=j and i!=3]
-        fig,ax1 = plt.subplots()
+        fig,ax1 = plt.subplots(figsize=(6.4/0.63,4.8))
         ax1.set_xlabel("Distance (meters)")
         ax1.set_ylabel("Normalized SSTDR Magnitude")
         std_devs = np.std(Y,axis=0)
@@ -273,15 +306,16 @@ for i,(name,Y) in enumerate(zip(("Illuminance","Temperature","Humidity","All"),(
         mean = np.mean(Y,axis=0)
         upper = mean+2*std_devs
         lower = mean-2*std_devs
-        area = scipy.integrate.simps(abs(upper-lower))
+        area = scipy.integrate.simps(abs(upper-lower)) #TODO make this use meters as an X axis, not samples
         upper_l, = ax1.plot(meters, upper,lw=lw,label="Upper Bound",color="C1")
         mean_l,  = ax1.plot(meters,  mean,lw=lw,label="Simulated Mean",color="C0")
         lower_l, = ax1.plot(meters, lower,lw=lw,label="Lower Bound",color="C2")
         ax1.fill_between(x=meters,y1=upper,y2=lower,alpha=0.3)
         ax1.legend(loc='upper right')
         ax1.set_ylim((-1,1))
-        if i!=3: ax1.set_title("Variation Due to {:s}\n{:s}\n{:s}\nShaded Area: {:.2f}".format(name,used_m_strings[0],used_m_strings[1], area))
-        else: ax1.set_title("Variation Due to {:s}\n\n\nShaded Area: {:.2f}".format(name, area))
+        #if i!=3: ax1.set_title("Variation Due to {:s}\n{:s}\n{:s}\nShaded Area: {:.2f}".format(name,used_m_strings[0],used_m_strings[1], area))
+        #else: ax1.set_title("Variation Due to {:s}\n\n\nShaded Area: {:.2f}".format(name, area))
+        ax1.set_title("Variation Due to {:s}; Shaded Area = {:.2f}".format(name, area))
         ax2 = ax1.twinx()
         ax2.plot(meters,std_devs,lw=lw, linestyle=':',color='purple')
         ax2.set_ylabel("Std. Dev")
@@ -295,6 +329,8 @@ for i,(name,Y) in enumerate(zip(("Illuminance","Temperature","Humidity","All"),(
         ax2.grid(True, which='both',axis='y')
         plt.tight_layout()
         plt.savefig("{:s}/variation_{:s}.png".format(out_folder,name.lower()))
+plt.rcParams.update({'font.size': FONT_SIZE})
+
 
 #CC vs environment factors plots
 illum_cc_poly = np.poly1d(np.polyfit(illum,p_cc,1))
@@ -411,7 +447,6 @@ if REFLECTION_PLOT:
     #ax1.set_ylim((-1,1))
     #ax1.set_xlim((np.min(degC),np.max(degC)))
     ax1.set_title("Expected Values at {:.1f} Meters".format(meters[location_index]))
-    #TODO plot fault reflections
     """
     ax2 = ax1.twinx()
     ax2.plot(meters,std_devs,lw=lw, linestyle=':',color='purple')
@@ -429,8 +464,27 @@ if REFLECTION_PLOT:
 
 #new idea: M slope
 if REFLECTION_PLOT:
-    locations = (40, 65, 70)
-    location_indices = [get_closest(meters, l) for l in locations] #get index of closest location
+    #locations = (40, 65, 70)
+    #location_indices = [get_closest(meters, l) for l in locations] #get index of closest location
+    #M_list = [M[:,i] for i in location_indices]
+    
+    M_names = ["Median Pre-panel", "Median Post-Panel", "All Locations"]
+    range_points = (get_closest(meters, 0), get_closest(meters, 60), len(meters))
+    M_list = []
+    M_list.append(np.median(np.abs(M[:,range_points[0]:range_points[1]]),axis=1)) #pre-panel slope mean
+    M_list.append(np.median(np.abs(M[:,range_points[1]:range_points[2]]),axis=1)) #post-panel slope mean
+    M_list.append(np.max(M, axis=1)) #highest slope for any point for each factor
+    
+    #PAPER TODO
+    #[x] means and max is fine, consider adding numbers to fault labels, add labels for each colored line (make sure in order)
+    #[x] make plot taller?
+    #[x] make figure 4 span entire column
+    #[ ] other plot: number baselines required vs each range; required to catch every tkind of fault
+    #[x] citations: remove ISBNs, DOI/URLS
+    #[x] use time instead of samples in figure 3
+    #[ ] we want to automate environment data collection... consider long usb cable to connect the sensor to the cupola computer
+    
+    #[ ] Fix variation reduction: splits into 2*N portions rather than N+1 portions
     
     reflection_N = 500
     samples = np.linspace(0,100,reflection_N)
@@ -440,6 +494,7 @@ if REFLECTION_PLOT:
     rh_range    = np.linspace(0,np.max(RH)-np.min(RH),reflection_N)
     
     fig,axes = plt.subplots(3,1,sharey=True, constrained_layout=True)
+    fig.set_figheight(1.5*fig.get_figheight())
     #outline_ax = fig.add_subplot(111,frameon=False)
     #outline_ax.set_ylabel("Expected Variation")
     #outline_ax.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
@@ -453,19 +508,23 @@ if REFLECTION_PLOT:
     axes[1].set_xlim((0,degC_range[-1]))
     axes[2].set_xlim((0,rh_range[-1]))
     
-    axes[0].set_yticks((0,0.1,0.2))
-    axes[1].set_yticks((0,0.1,0.2))
-    axes[2].set_yticks((0,0.1,0.2))
+    axes[0].set_yticks((0,0.2,0.4))
+    axes[1].set_yticks((0,0.2,0.4))
+    axes[2].set_yticks((0,0.2,0.4))
     
     axes[1].set_ylabel("SSTDR Variation")#don't make this too long or the subplots get squished
-    
-    for location_index in location_indices:
+    handles = [None]*3
+    for i,m in enumerate(M_list):
         #plot line of slope M[n] starting at 0. each n represents a particular factor.
-        axes[0].plot(illum_range, illum_range*abs(M[0,location_index]), lw=2, label="Illum Range @ "+str(location_index))
-        axes[1].plot(degC_range, degC_range*abs(M[1,location_index]), lw=2, label="Temperature Range @ "+str(location_index))
-        axes[2].plot(rh_range, rh_range*abs(M[2,location_index]), lw=2, label="Humidity Range @ "+str(location_index))
-    
-    #ax1.legend(loc='lower right')
+        #axes[0].plot(illum_range, illum_range*abs(m[0]), lw=2, label="Illum Range @ "+str(location_index))
+        handles[i], = axes[0].plot(illum_range, illum_range*abs(m[0]), lw=2)
+        #axes[1].plot(degC_range, degC_range*abs(m[1]), lw=2, label="Temperature Range @ "+str(location_index))
+        axes[1].plot(degC_range, degC_range*abs(m[1]), lw=2)
+        #axes[2].plot(rh_range, rh_range*abs(m[2]), lw=2, label="Humidity Range @ "+str(location_index))
+        axes[2].plot(rh_range, rh_range*abs(m[2]), lw=2)
+    handles.reverse()
+    M_names.reverse()
+    axes[0].legend(handles, M_names, loc='upper left')
     #axes[0].set_ylim((0,1))
     #axes[1].set_ylim((0,1))
     #axes[2].set_ylim((0,1))
@@ -497,8 +556,9 @@ if REFLECTION_PLOT:
         
         #plot label text
         for f,n in zip(norm_faults, sorted_names):
+            t = "{:.2f}: {:s}".format(f,n)
             if f < twin.get_ylim()[1]:
-                texts.append(axes[i].text(xmax+text_x_padding*xmax,f,n,va='center',ha='left')) #normalize padding
+                texts.append(axes[i].text(xmax+text_x_padding*xmax,f,t,va='center',ha='left')) #normalize padding
         #arrange label text
         r = fig.canvas.get_renderer()
         text_height = texts[0].get_window_extent(renderer=r).inverse_transformed(axes[i].transData).height #height of text in plot units, not pixels
@@ -516,6 +576,69 @@ if REFLECTION_PLOT:
 
     #plt.tight_layout()
     plt.savefig("{:s}/variation_comparison.png".format(out_folder))
+
+
+#======== baselines required plot ============
+#fig,axes = plt.subplots(3,1,sharey=True, constrained_layout=True)
+fig,axes = plt.subplots(3,1,sharey=True)
+fig.set_figheight(1.5*fig.get_figheight())
+
+axes[0].set_xlabel("Maximum Illuminance Range [Δ log10 Lux]")
+axes[1].set_xlabel("Maximum Temperature Range [Δ °C]")
+axes[2].set_xlabel("Maximum Humidity Range [Δ %]")
+
+for ax in axes:
+    ax.set_yticks([1,3,5,7,9],minor=False)
+    ax.set_yticks([3,5,7],minor=True)
+
+axes[0].set_xlim((0,illum_range[-1]))
+axes[1].set_xlim((0,degC_range[-1]))
+axes[2].set_xlim((0,rh_range[-1]))
+
+outline_ax = fig.add_subplot(111,frameon=False)
+outline_ax.set_ylabel("Minimum Baselines Required s.t. Variation ≤ Faults")
+outline_ax.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+
+#fig.subplots_adjust(hspace=0,wspace=0)
+handles = [None]*3
+for i,m in enumerate(M_list):
+    #plot line of slope M[n] starting at 0. each n represents a particular factor.
+    #old formula: n = ceil(range / (smallest_fault_variation / slope)) - 1
+    #assumed N baselines split the range into N+1 pieces,
+    #producing variation equal to the full range variation / (N+1)
+    #incorrectly allocates left/right sides of baselines 'territory' to overlap.
+    #num_illum = np.ceil((illum_range / (norm_faults[0] / m[0])) - 1)
+    #num_degC  = np.ceil(( degC_range / (norm_faults[0] / m[1])) - 1)
+    #num_rh    = np.ceil((   rh_range / (norm_faults[0] / m[2])) - 1)
+    
+    #new formula: n = ceil(slope*range/2/smallest_fault)
+    #assumes N baselines split the variation into N regions, with each baseline at the center,
+    #producing variation equal to the full range variation / N / 2. algebra steps below
+    #full_var = slope*range
+    #partial_var = slope*partial_range = slope*range/N/2
+    #smallest_fault >= slope*range/N/2
+    #N >= slope*range/2/smallest_fault
+    
+    num_illum = np.ceil(m[0]*illum_range/2/norm_faults[0])
+    num_degC  = np.ceil(m[1]* degC_range/2/norm_faults[0])
+    num_rh    = np.ceil(m[2]*   rh_range/2/norm_faults[0])
+    
+    num_illum[num_illum < 1] = 1
+    num_degC [num_degC  < 1] = 1
+    num_rh   [num_rh    < 1] = 1
+    handles[i], = axes[0].plot(illum_range, num_illum, lw=2)
+    axes[1].plot(degC_range, num_degC, lw=2)
+    axes[2].plot(rh_range, num_rh, lw=2)
+handles.reverse()
+axes[0].legend(handles,M_names, loc='upper left')
+
+axes[0].grid(True, which='both',axis='y')
+axes[1].grid(True, which='both',axis='y')
+axes[2].grid(True, which='both',axis='y')
+
+plt.tight_layout()
+plt.savefig("{:s}/baseline_requirements.png".format(out_folder))
+
 
 #========== PAST-INFORMED LINEAR MODEL =============
 if PAST_PLOT:    
